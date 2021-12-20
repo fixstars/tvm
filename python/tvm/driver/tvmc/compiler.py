1d58c8216f0c95d53399135c6601510bf4af4f65
Yasuhiro Nitta <yasuhiro.nitta@fixstars.com>
2021-12-20 02:37:28 +0000

    Revert "Apply config from partition"
    This reverts commit ed7971d3433840dad7790d9381061a54601446c7.

--- python/tvm/driver/tvmc/compiler.py
+++ python/tvm/driver/tvmc/compiler.py
@@ -243,7 +243,7 @@ def compile_model(
         partition_function = codegen["pass_pipeline"]
         mod = partition_function(mod, params, **codegen_from_cli["opts"])
         if codegen["config_key"] is not None:
-            config[codegen["config_key"]] = codegen_from_cli["opts"] or codegen_config
+            config[codegen["config_key"]] = codegen_from_cli["opts"]
 
     if tuning_records and os.path.exists(tuning_records):
         logger.debug("tuning records file provided: %s", tuning_records)
